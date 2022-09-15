c4fcfa8fb22fb60f2ac189cc6e97711c1b9c31f6
zainabaljifri <80160006+zainabaljifri@users.noreply.github.com>
2022-09-15 21:14:25 +0300

    v2

--- js/app.js
+++ js/app.js
@@ -1,22 +1,57 @@
-$(document).ready(function() {
-  let path = window.location.pathname;
-  $('.nav-link').each(function() {
-    if (path === $(this).attr('href')) {
-      $(this).addClass('active');
-    }
-  });
-});
-
 function validateForm() {
   let name = document.forms["form"]["name"].value;
   let email = document.forms["form"]["email"].value;
   if (name == "") {
-    swal("Name must be filled out");
-    document.form.name.focus() ;
+    Swal.fire({
+  icon: 'error',
+  title: 'Oops...',
+  text: 'Name must be filled out'
+}).then((result) => {
+    $('#name').focus();
+    $('#name').focus();
+    });
     return false;
   } else if (email == "") {
-    swal("Email must be filled out");
-    document.form.email.focus() ;
+    Swal.fire({
+  icon: 'error',
+  title: 'Oops...',
+  text: 'Email must be filled out'
+}).then((result) => {
+    $('#name').focus();
+    $('#name').focus();
+    });
     return false;
   }
+}
+
+function darkMode(){
+  $("body").toggleClass("dark");
+  $("#navbar").toggleClass("dark");
+  $(".icon").toggleClass("dark-fill");
+  $(".white, .card__overlay, .card__header, .nav-link").toggleClass("dark");
+  $(".nav-link").hover(function() {
+    $( this ).toggleClass( "dark" );
+  });
+  $(".card").toggleClass("dark-card");
+  $(".arc").css({ fill: '#292c35' });
+  $(".input").toggleClass("dark-input");
+
+
+}
+
+$('#checkbox').on('change',function(){
+  darkMode();
+    if ($('#checkbox').is(":checked")) {
+      localStorage.setItem("mode", "dark");
+
+      }
+    else {
+      localStorage.setItem("mode", "light");
+      $(".arc").css({ fill: 'white' });
+    }
+  });
+
+if (localStorage.getItem("mode") === "dark") {
+  $('#checkbox').prop('checked', true);
+  darkMode();
 }
